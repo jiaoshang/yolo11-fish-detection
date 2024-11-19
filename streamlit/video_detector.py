62e139738af717d8d63fa21c794ad213e70b7e58
Chongze Jiao <cjiao@bol.com>
2024-11-19 21:53:58 +0100

    add a button to stop/restart the video

--- streamlit/video_detector.py
+++ streamlit/video_detector.py
@@ -14,26 +14,34 @@ def _display_detected_frame(file_path: str):
     capture = cv2.VideoCapture(file_path)  # TODO whether it's possible for capture from cache
     st_frame = st.empty()
     while capture.isOpened():
-        success, frame = capture.read()
-        # Resize the image to a standard size
-        # frame = cv2.resize(frame, (720, int(720 * (9 / 16))))
-        if not success:
-            print("Video frame is empty or video processing has been successfully completed.")
-            break
-        processed_frame = model.predict(frame)
-        count = len(processed_frame[0])
-        annotated_frame = processed_frame[0].plot()  # Visualize the results on the frame
-        cv2.putText(annotated_frame, f"fish: {count}", (0, 1024), cv2.FONT_HERSHEY_SIMPLEX,
-                    1.25, (255, 255, 255), 3)
-        st_frame.image(annotated_frame,
-                       caption='Detected Video',
-                       channels="BGR",
-                       use_container_width=True
-                       )
+        if not st.session_state.state:
+            success, frame = capture.read()
+            # Resize the image to a standard size
+            # frame = cv2.resize(frame, (720, int(720 * (9 / 16))))
+            if not success:
+                print("Video frame is empty or video processing has been successfully completed.")
+                break
+            processed_frame = model.predict(frame)
+            count = len(processed_frame[0])
+            annotated_frame = processed_frame[0].plot()  # Visualize the results on the frame
+            cv2.putText(annotated_frame, f"fish: {count}", (0, 1024), cv2.FONT_HERSHEY_SIMPLEX,
+                        1.25, (255, 255, 255), 3)
+            st_frame.image(annotated_frame,
+                           caption='Detected Video',
+                           channels="BGR",
+                           use_container_width=True
+                           )
     capture.release()
+
 
 with st.sidebar:
     uploaded_video_bytes = st.file_uploader("Choose an video", type=("mp4", "mov"))
+
+stop_restart = st.button("Stop/Restart", key="stop_button")
+if "state" not in st.session_state:
+    st.session_state.state = False
+if stop_restart:
+    st.session_state.state = not st.session_state.state
 
 try:
     # model = load_model_as_object_counter("streamlit/model.pt") # TODO: think about the relative path later
@@ -45,5 +53,3 @@ except Exception as ex:
 if uploaded_video_bytes is not None:
     video_file_path = _save_uploaded_video(uploaded_video_bytes)
     _display_detected_frame(video_file_path)
-
-# TODO check whether it's possible to pause the detection on video
