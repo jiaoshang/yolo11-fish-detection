7d61d296a35fc36127bef5b69c9f7ebf20047ee3
Chongze Jiao <cjiao@bol.com>
2024-11-19 21:30:49 +0100

    deal with NameError: name 'xxx' is not defined

--- streamlit/image_detector.py
+++ streamlit/image_detector.py
@@ -13,20 +13,21 @@ except Exception as ex:
     st.error("Can't load YOLO model")
     st.error(ex)
 
-# TODO setup default image to deal with NameError: name 'uploaded_image' is not defined
 col1, col2 = st.columns(2)
 
 with col1:
     try:
-        uploaded_image = Image.open(source_img)
-        st.image(source_img, caption="Uploaded Image", use_container_width=True)
+        if source_img is not None:
+          uploaded_image = Image.open(source_img)
+          st.image(source_img, caption="Uploaded Image", use_container_width=True)
     except Exception as ex:
         st.error("Error occurred while opening the image.")
         st.error(ex)
 
     with col2:
-        res = model.predict(uploaded_image) #, conf=confidence) # TODO: add more parameters
-        boxes = res[0].boxes
-        res_plotted = res[0].plot()[:, :, ::-1]
-        st.image(res_plotted, caption='Detected Image', use_container_width=True)
-        st.info(f'Number of fishes: {len(res[0])}', icon="ℹ️")
+        if source_img is not None:
+            res = model.predict(uploaded_image) #, conf=confidence) # TODO: add more parameters
+            boxes = res[0].boxes
+            res_plotted = res[0].plot()[:, :, ::-1]
+            st.image(res_plotted, caption='Detected Image', use_container_width=True)
+            st.info(f'Number of fishes: {len(res[0])}', icon="ℹ️")
--- streamlit/video_detector.py
+++ streamlit/video_detector.py
@@ -4,11 +4,33 @@ import streamlit as st
 from model import load_model_as_object_counter, load_model
 
 
-def _save_uploaded_video(uploaded_video_bytes) -> str:
-    file_path = "streamlit/data/uploaded_video/upload." + uploaded_video_bytes.name.split('.')[-1]
+def _save_uploaded_video(video_bytes) -> str:
+    file_path = "streamlit/data/uploaded_video/upload." + video_bytes.name.split('.')[-1]
     with open(file_path, 'wb') as out:
-        out.write(uploaded_video_bytes.read())
+        out.write(video_bytes.read())
     return file_path
+
+def _display_detected_frame(file_path: str):
+    capture = cv2.VideoCapture(file_path)  # TODO whether it's possible for capture from cache
+    st_frame = st.empty()
+    while capture.isOpened():
+        success, frame = capture.read()
+        # Resize the image to a standard size
+        # frame = cv2.resize(frame, (720, int(720 * (9 / 16))))
+        if not success:
+            print("Video frame is empty or video processing has been successfully completed.")
+            break
+        processed_frame = model.predict(frame)
+        count = len(processed_frame[0])
+        annotated_frame = processed_frame[0].plot()  # Visualize the results on the frame
+        cv2.putText(annotated_frame, f"fish: {count}", (0, 1024), cv2.FONT_HERSHEY_SIMPLEX,
+                    1.25, (255, 255, 255), 3)
+        st_frame.image(annotated_frame,
+                       caption='Detected Video',
+                       channels="BGR",
+                       use_container_width=True
+                       )
+    capture.release()
 
 with st.sidebar:
     uploaded_video_bytes = st.file_uploader("Choose an video", type=("mp4", "mov"))
@@ -20,26 +42,8 @@ except Exception as ex:
     st.error("Can't load YOLO model")
     st.error(ex)
 
-video_file_path = _save_uploaded_video(uploaded_video_bytes)
-capture = cv2.VideoCapture(video_file_path) # TODO whether it's possible for capture from cache
-st_frame = st.empty()
-while capture.isOpened():
-    success, frame = capture.read()
-    # Resize the image to a standard size
-    # frame = cv2.resize(frame, (720, int(720 * (9 / 16))))
-    if not success:
-        print("Video frame is empty or video processing has been successfully completed.")
-        break
-    processed_frame = model.predict(frame)
-    count = len(processed_frame[0])
-    annotated_frame = processed_frame[0].plot() # Visualize the results on the frame
-    cv2.putText(annotated_frame, f"fish: {count}", (0, 1024), cv2.FONT_HERSHEY_SIMPLEX,
-                1.25, (255, 255, 255), 3)
-    st_frame.image(annotated_frame,
-                   caption='Detected Video',
-                   channels="BGR",
-                   use_container_width=True
-                   )
-capture.release()
+if uploaded_video_bytes is not None:
+    video_file_path = _save_uploaded_video(uploaded_video_bytes)
+    _display_detected_frame(video_file_path)
 
 # TODO check whether it's possible to pause the detection on video
